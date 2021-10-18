1af28ede0ea456fb886bbb99466328a2cfbd4a50
victorsk2019 <47955466+victorsk2019@users.noreply.github.com>
2021-10-18 11:58:00 -0700

    More changes to classes.

--- src/com/rpclib/util/RPCConnect.java
+++ src/com/rpclib/util/RPCConnect.java
@@ -1,5 +1,10 @@
 package com.rpclib.util;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  *
  * @author Victor Skovorodnikov
@@ -15,4 +20,26 @@ public class RPCConnect
         return response;
     }
     
+    private String processRequest(RPCProperties props, String method)
+    {
+        try
+        {
+            URL cURL = new URL("http", props.getRPCIP(), props.getRPCPort(), "");
+            HttpURLConnection conn = (HttpURLConnection)cURL.openConnection();
+            
+            
+        }
+        
+        catch (MalformedURLException me)
+        {
+            me.printStackTrace();
+        }
+        
+        catch (IOException ie)
+        {
+            
+        }
+        
+        return "";
+    }
 }
