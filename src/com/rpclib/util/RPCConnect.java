2a923966bbc8c63171f7548a07459399f0254a00
victorsk2019 <47955466+victorsk2019@users.noreply.github.com>
2021-10-19 10:28:23 -0700

    More changes to classes.

--- src/com/rpclib/util/RPCConnect.java
+++ src/com/rpclib/util/RPCConnect.java
@@ -1,9 +1,17 @@
 package com.rpclib.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -16,16 +24,57 @@ public class RPCConnect
     
     public String getResponse(RPCProperties props, String method)
     {
-        String response = "";
-        return response;
+        System.out.println("Trying to get some response");
+        return processRequest(props, method);
     }
     
     private String processRequest(RPCProperties props, String method)
     {
+        String RPCResponse = "";
+        
         try
         {
-            URL cURL = new URL("http", props.getRPCIP(), props.getRPCPort(), "");
+            URL cURL = new URL("http://" + props.getRPCIP() + ":" + props.getRPCPort());
+            String RQ = getJSONContent(method);
             HttpURLConnection conn = (HttpURLConnection)cURL.openConnection();
+            
+            String authParams = props.getRPCUser() + ":" + props.getRPCPassword();
+            conn.setRequestProperty("Authorization", "Basic " + new String(Base64.getEncoder().encode(authParams.getBytes())));
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            
+            OutputStream os = conn.getOutputStream();
+            os.write(RQ.getBytes("UTF-8"), 0 , RQ.length());
+            
+            
+            try
+            {
+                Map <String, List<String>> map = conn.getHeaderFields();
+                map.forEach((k, v) -> System.out.println(k + " " + v));           
+            }
+            
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+           
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            
+            while ((inputLine = in.readLine()) != null) 
+            {
+                System.out.println("---> " + inputLine);
+                RPCResponse += inputLine;
+            }
+            
+            in.close();
+            
+            os.flush();
+            os.close();
+            conn.disconnect();
+            
             
             
         }
@@ -37,9 +86,14 @@ public class RPCConnect
         
         catch (IOException ie)
         {
-            
+            ie.printStackTrace();
         }
         
-        return "";
+        return RPCResponse;
+    }
+    
+    private String getJSONContent(String method)
+    {
+        return "{\"jsonrpc\":\"1.0\", \"id\":\"curltest\", \"method\": \"" + method + "\", \"params\": [] }";
     }
 }
